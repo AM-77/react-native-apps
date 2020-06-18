a2ee3a141764db4064f0cef0c8ad519908fab76a
AM-77 <amine.griche77@protonmail.ch>
2020-06-18 23:42:04 +0200

    add colors

---
+++ thoughts/assets/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  main: '#06C270', 
+  dark: '#121212', 
+  light: '#f1f2f3', 
+  black: '#000000', 
+  white: '#ffffff', 
+  gray: '#363636',
+  red: '#f44336'
+}
+
+export default colors
